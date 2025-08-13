c95a30190d0ea7f886cf61c03b016ce48957f06a
keirrisongreco <keirrison.greco@escola.pr.gov.br>
2025-08-13 07:37:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ let container = document.getElementById('container')
   cartao.className = 'cartao'
 
   cartao.innerHTML = `
+    <div class="cartao_conteudo">
+                    <h3>filmes</h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <p>quais são os filme que mais ganhou premios no mundo?</p>
+
+                    </div>
+                    <div class="cartao_conteudo_resposta">
+                        <p>Ben-Hur, Titanic, O Senhor dos Anéis: O Retorno do Rei todos detem 11 estatuetas cada!</p>
+                    </div>
+                </div>
+                `
 }
