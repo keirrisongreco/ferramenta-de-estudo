cb3d51c7799ae11a7bac45fc19e8a5ad3295f672
keirrisongreco <keirrison.greco@escola.pr.gov.br>
2025-08-13 07:35:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function criaCartao(categoria, pergunta, resposta) {
-  console.log(categoria, pergunta, resposta)
+let container = document.getElementById('container')
+  let cartao = document.createElement('article')
+  cartao.className = 'cartao'
+
+  cartao.innerHTML = `
 }
