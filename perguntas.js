156b3002d9d22d3d7a93b9c51e5572f45e44da69
keirrisongreco <keirrison.greco@escola.pr.gov.br>
2025-08-20 07:27:24 -0300

    novo cod atualizado

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'filmes',
     'quais são os filme que mais ganhou premios no mundo?',
-    'Ben-Hur, Titanic, O Senhor dos Anéis: O Retorno do Rei todos detem 11 estatuetas cada!',
+    'Ben-Hur, Titanic, O Senhor dos Anéis: O Retorno do Rei todos detem 11 estatuetas cada!'
 )
 
 criaCartao(
     'ator',
     'qual ator fez mais filmes no mundo?',
-    'o ator que tem mais filmes no mundo é o ator Franklyn Farnum',
+    'o ator que tem mais filmes no mundo é o ator Franklyn Farnum'
 )
 
 criaCartao(
     'oscars',
     'qual franquia de filmes que tem mais oscars',
-    'a franquia de filmes que teve mais oscars foi "senhor dos aneis',
+    'a franquia de filmes que teve mais oscars foi "senhor dos aneis'
 )
 
 criaCartao(
     'series',
     'qual série de ficção científica de maior sucesso?',
-    'Docthor who',
+    'Docthor who'
 )
